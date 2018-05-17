a89a19d6f14561dbd8894828fec6f2844a5f2fc4
minberryvery <minberryvery@naver.com>
2018-05-17 21:52:37 +0900

    mecab-ffi 추가

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 'use strict'
-
 const Hapi = require('hapi')
+const mecab = require('mecab-ffi')
 
 const server = Hapi.server({
     host: '0.0.0.0',
     port: 8002
 })
+
+server.route({
+    method: 'GET',
+    path: '/{text}',
+    handler: (req, h) => {
+        return getParse(encodeURIComponent(req.params.text))
+    }
+})
+
+async function getParse(text) {  
+    let result = await mecab.parse(text, (err, body) => {
+        return body
+    })
+    return result
+}
+
 
 const init = async () => {
     await server.start()
